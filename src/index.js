8759001a22d1d8586f2a95e31a6cd1324297b8c5
rythm-sachdeva <rythmrko123@gmail.com>
2024-05-22 21:36:43 +0530

    Creation of a Few Utilities

--- src/index.js
+++ src/index.js
@@ -4,4 +4,12 @@ import connectDB from "./db/index.js";
 dotenv.config({
     path: './env'
 })
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`process is running at port : ${process.env.PORT}`)
+    })
+})
+.catch((err)=> {
+    console.log("Mongoose Connection Failed!!",err)
+})
