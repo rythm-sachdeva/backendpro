9e54676c59b8e7c2c9d9178414a8bdbe34eb2f7f
rythm-sachdeva <rythmrko123@gmail.com>
2024-05-29 06:30:33 +0530

    Whole app configures and bugs fixed

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import cookieParser from "cookie-parser";
+import cors from "cors"
 import express  from "express";
 
 const app= express();
@@ -11,4 +12,11 @@ app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended: true,limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+//Routes
+import userRouter from './routes/user.routes.js'
+
+//Route MiddleWare
+app.use('/api/v1/users',userRouter)
+
 export {app}
