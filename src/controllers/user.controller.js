382df0bfdeb36ffb225c332de7d49b5a2586d40b
rythm-sachdeva <rythmrko123@gmail.com>
2024-06-03 21:07:27 +0530

    Added Aggregation pipelines to calculate subscribers

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -299,6 +299,78 @@ const updateCoverImage = asyncHandler( async (req,res) => {
 
 } )
 
+const getUserChannelProfile = asyncHandler( async (req,res)=>{
+
+    const {username} = req.params
+
+    if(!username?.trim()){
+        throw new Apierror(400,"Username is Missing")
+    }
+
+   const channel = await User.aggregate([
+        {
+            $match:{
+                username: username?.toLowerCase()
+            }
+        },
+        {
+            $lookup:{
+                from:"subscriptions",
+                localField:"_id",
+                foreignField:"channel",
+                as:"subscribers"
+            }
+        },
+        {
+            $lookup:{
+                from:"subscriptions",
+                localField:"_id",
+                foreignField:"subscriber",
+                as:"subscribed-to"
+            }
+        },
+        {
+            $addFields: {
+                subscribersCount:{
+                    $size: "$subscribers"
+                },
+                channelsSubscribedTo:{
+                    $size:"$subscribed-to"
+                },
+                isSubscribed: {
+                    $cond: {
+                        if: {$in: [req.user?._id, "$subscribers.subscriber"]},
+                        then: true,
+                        else:false
+                    }
+                }
+            }
+        },
+        {
+            $project:{
+                fullname:1,
+                username:1,
+                subscribersCount:1,
+                channelsSubscribedTo:1,
+                isSubscribed:1,
+                avatar:1,
+                coverImage:1,
+                email:1
+
+
+            }
+        }
+
+    ])
+    if(!channel?.length)
+        {
+            throw new Apierror(404,"channel Does Not Exist")
+        }
+        return res.status(200).json(new Apiresponse(200,channel[0],"Channel Does Not Exist"))
+})
+
+
+
 export {
     registerUser,
     loginUser,
