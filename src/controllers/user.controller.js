5c3e551195a85a157992580df1bbef87069d43d7
rythm-sachdeva <rythmrko123@gmail.com>
2024-08-09 18:45:21 +0530

    fixed logout bug

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -129,8 +129,8 @@ const loginUser = asyncHandler(
 const logoutUser = asyncHandler(async (req,res) =>{
   await User.findByIdAndUpdate(
     req.user._id,{
-        $set:{
-            refreshToken:undefined
+        $unset:{
+            refreshToken:1
         }
     },
     {
